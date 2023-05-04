d73c4f633016624408e8aedb1bacda058e386842
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2023-05-04 20:21:01 -0300

    Se creo componente Cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/Navbar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Cards from './components/Cards/Cards';
 
 function App() {
   return (
     <>
       <NavBar />
       <ItemListContainer greeting={'Aca no juzgamos a nadie !'} />
+      <Cards />
     </>
   );
 }
